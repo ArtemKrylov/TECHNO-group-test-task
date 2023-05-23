ce5a20a89e1b4eeb11f9495297e2419d364c8834
Artem <43321337+ArtemKrylov@users.noreply.github.com>
2023-05-23 14:35:34 +0300

    initial

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,11 @@
-import React,{ FC }  from 'react';
+import React, { FC } from 'react';
 import { GlobalStyle } from '../GlobalStyle';
 
-
-const App:FC = () => {
+const App: FC = () => {
   return (
     <div className="app">
       <GlobalStyle />
     </div>
   );
-}
+};
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
-import reportWebVitals from './reportWebVitals';
 
 import { theme } from 'utils/constants/theme';
 import App from './components/App/App';
@@ -19,8 +18,3 @@ root.render(
     </ThemeProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
