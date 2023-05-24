0b81ac0ff2086f286d61e4c26405fc619c61fcd3
Artem <43321337+ArtemKrylov@users.noreply.github.com>
2023-05-24 20:38:20 +0300

    added env and vercel, changed pool - to deploy to vercel

--- server/db.js
+++ server/db.js
@@ -1,11 +1,15 @@
 const Pool = require("pg").Pool;
 
+// const pool = new Pool({
+//   user: "postgres",
+//   password: "3252223",
+//   host: "localhost",
+//   port: 5432,
+//   database: "deliveryapp",
+// });
+
 const pool = new Pool({
-  user: "postgres",
-  password: "3252223",
-  host: "localhost",
-  port: 5432,
-  database: "deliveryapp",
+  connectionString: process.env.POSTGRES_URL + "?sslmode=require",
 });
 
 module.exports = pool;
