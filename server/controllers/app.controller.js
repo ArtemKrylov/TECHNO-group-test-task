25361362da93107fc68c2b6e0b4d72b204851093
Artem <krylovartem1994@gmail.com>
2023-07-21 18:31:27 +0300

    feat: added routes, appController

--- server/controllers/app.controller.js
+++ server/controllers/app.controller.js
@@ -1,64 +1,29 @@
 const postgre = require("../database");
 
 const appController = {
-  //get all shops
-  // getAll: async (req, res) => {
-  //   try {
-  //     const { rows } = await postgre.query("SELECT * FROM public.shops");
-  //     res.json(rows);
-  //   } catch (error) {
-  //     console.error(error);
-  //   }
-  // },
-  //get all products from the shop
-  // getProductsByShopId: async (req, res) => {
-  //   const { id } = req.params;
-  //   try {
-  //     const products = await postgre.query(
-  //       "SELECT * FROM public.products WHERE shop_id = $1",
-  //       [id]
-  //     );
-  //     res.json(products.rows);
-  //   } catch (error) {
-  //     console.error(error);
-  //   }
-  // },
-  //create order
-  // createOrder: async (req, res) => {
-  //   try {
-  //     console.log(req.body);
-  //     const {
-  //       name,
-  //       email,
-  //       phone,
-  //       customer_address,
-  //       shop_id,
-  //       order_items,
-  //       total_price,
-  //     } = req.body;
+  //get all clients
+  getClients: async (req, res) => {
+    try {
+      const { rows } = await postgre.query("SELECT * FROM public.shops");
+      res.json(rows);
+    } catch (error) {
+      console.error(error);
+    }
+  },
 
-  //     const newOrder = await postgre.query(
-  //       `INSERT INTO orders (name,
-  //       email,
-  //       phone,
-  //       customer_address,	
-  //       shop_id, total_price,
-  //       order_items) VALUES($1,$2,$3,$4,$5,$6,$7)`,
-  //       [
-  //         name,
-  //         email,
-  //         phone,
-  //         customer_address,
-  //         shop_id,
-  //         total_price,
-  //         order_items,
-  //       ]
-  //     );
-  //     res.json(newOrder);
-  //   } catch (error) {
-  //     console.error(error);
-  //   }
-  // },
+  //get all client`s projects
+  getClientProjects: async (req, res) => {
+    const { clientId } = req.params;
+    try {
+      const products = await postgre.query(
+        "SELECT * FROM public.products WHERE shop_id = $1",
+        [clientId]
+      );
+      res.json(products.rows);
+    } catch (error) {
+      console.error(error);
+    }
+  },
 };
 
 module.exports = appController;
--- server/routes/app.router.js
+++ server/routes/app.router.js
@@ -3,8 +3,7 @@ const router = express.Router();
 
 const appController = require("../controllers/app.controller");
 
-// router.get("/shops", appController.getAll);
-// router.get("/products/:id", appController.getProductsByShopId);
-// router.post("/orders/make-order", appController.createOrder);
+router.get("/clients", appController.getClients);
+router.get("/:clientId/projects", appController.getClientProjects);
 
 module.exports = router;
