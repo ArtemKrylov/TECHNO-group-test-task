0ea3d95281817d660e161aeca356eb7c201fe9c5
Artem <krylovartem1994@gmail.com>
2023-07-23 20:58:14 +0300

    bugfix: added cors headers

--- server/index.js
+++ server/index.js
@@ -5,6 +5,15 @@ const cors = require("cors");
 require("dotenv").config();
 
 app.use(cors());
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://techno-group-test-task.vercel.app"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 app.use(express.json());
 
 const appRouter = require("./routes/app.router.js");
