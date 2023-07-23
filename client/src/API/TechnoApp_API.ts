2ede7d9059b5e677f84d7b9b6079815bb064aa8b
Artem <krylovartem1994@gmail.com>
2023-07-23 20:47:39 +0300

    bugfix: changed baseURL for axios to vercel url

--- client/src/API/TechnoApp_API.ts
+++ client/src/API/TechnoApp_API.ts
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:5000/api/v1';
+axios.defaults.baseURL =
+  'http://techno-group-test-task-express-psql-api/api/v1';
+// 'http://localhost:5000/api/v1';
 
 export const TechnoApp_API = {
   getClients: () => {
